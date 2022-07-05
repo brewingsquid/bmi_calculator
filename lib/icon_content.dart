e59051ddcad0d0569445c5b4341a12e214af4c3a
brewingsquid <brewingsquid@gmail.com>
2022-07-05 14:58:04 +0200

    correct fontawsome package issue

--- lib/icon_content.dart
+++ lib/icon_content.dart
@@ -23,7 +23,7 @@ class IconContent extends StatelessWidget {
           height: 15.0,
         ),
         Text(
-          label,
+          label!,
           style: labelTextStyle,
         ),
       ],
